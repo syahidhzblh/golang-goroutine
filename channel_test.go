09f48da90d52bc71aac1c857633284c8e29f248f
syahid <syahidhizbullah17@gmail.com>
2023-12-26 13:18:08 +0700

    update test config

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -37,4 +38,119 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 
 	time.Sleep(10 * time.Second)
+}
+
+// Func OnlyIn, hanya untuk mengirim ke channel
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Syahid"
+}
+
+// Func OnlyOut, hanya untuk menerima dari channel
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
+
+//Buffered Channel
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 2)
+	defer close(channel)
+
+	go func() {
+		channel <- "Syahid"
+		channel <- "Hizbullah"
+	}()
+
+	go func() {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
+	fmt.Println("Done")
+}
+
+// Range Channel, untuk menerima data yang tidak tahu jumlahnya berapa
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data_channel := range channel {
+		fmt.Println("Menerima Data", data_channel)
+	}
+}
+
+// Select Channel, untuk menerima data dari semua channel yang kita buat dalam goroutine
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	//For Infinity dengan Select Multiple Channel
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+// Default Select Channel
+func TestDefaultSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	//For Infinity dengan Select Multiple Channel
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari Channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari Channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu Data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+	fmt.Println("Selesai")
 }
